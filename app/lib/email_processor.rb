2f4b4e81d9742f5f251c33437d5bebb79d1d377b
William Kasel <wkasel@gmail.com>
2018-04-24 11:28:01 -0700

    updated to_json in mailer

--- app/lib/email_processor.rb
+++ app/lib/email_processor.rb
@@ -60,33 +60,13 @@ class EmailProcessor
         }
       ]
     }
-    # data = JSON.parse('{
-    #   "personalizations": [
-    #     {
-    #       "to": [
-    #         {
-    #           "email": "william@casualposts.com"
-    #         }
-    #       ],
-    #       "subject": "Reply For Post"
-    #     }
-    #   ],
-    #   "from": {
-    #     "email": "no-reply@reply.casualposts.com"
-    #   },
-    #   "content": [
-    #     {
-    #       "type": "text/plain",
-    #       "value": "and easy to do anywhere, even with Ruby"
-    #     }
-    #   ]
-    # }')
+
 
 
 
     sg = SendGrid::API.new(api_key: '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')
     # sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-    response = sg.client.mail._("send").post(request_body: data)
+    response = sg.client.mail._("send").post(request_body: data.to_json)
     puts response.status_code
     puts response.body
     puts response.parsed_body
